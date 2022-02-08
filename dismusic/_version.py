6dd6b032ef0507d162921de1c25de06435d350fc
shahriyar <mdshahriyaralam552@gmail.com>
2022-02-08 22:03:44 +0600

    Change version info and version number

--- dismusic/_version.py
+++ dismusic/_version.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
-__version__ = "1.1.1"
+__version__ = "1.1.1a"
 
 VersionInfo = namedtuple("VersionInfo", "major minor macro release")
 
-version_info = VersionInfo(*map(int, __version__.split(".")), "alpha")
+version_info = VersionInfo(1, 1, 1, "alpha")
