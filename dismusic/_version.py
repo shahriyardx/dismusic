b214c3ba9e044db3ce8125f8de3ed12e6ec948f0
shahriyar <mdshahriyaralam552@gmail.com>
2022-02-08 22:43:39 +0600

    Added typing-extensions as a requirement and improved versioninfo

--- dismusic/_version.py
+++ dismusic/_version.py
@@ -1,7 +1,16 @@
-from collections import namedtuple
+from dataclasses import dataclass
+from typing_extensions import Literal
 
-__version__ = "1.1.1a"
+__version__ = "1.1.1a0"
 
-VersionInfo = namedtuple("VersionInfo", "major minor macro release")
 
-version_info = VersionInfo(1, 1, 1, "alpha")
+@dataclass
+class VersionInfo:
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+version_info = VersionInfo(1, 1, 1, "alpha", 0)
