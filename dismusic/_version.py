59044344f86cf69840636b6df4d0c6d4caebadd9
shahriyar <mdshahriyaralam552@gmail.com>
2022-02-08 21:53:47 +0600

    Changed release to alpha

--- dismusic/_version.py
+++ dismusic/_version.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 VersionInfo = namedtuple("VersionInfo", "major minor macro release")
 
-version_info = VersionInfo(*map(int, __version__.split(".")), "stable")
+version_info = VersionInfo(*map(int, __version__.split(".")), "alpha")
