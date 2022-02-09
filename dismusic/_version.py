fd8308f80963d45212943ea6335ddd088def0460
shahriyar <mdshahriyaralam552@gmail.com>
2022-02-09 12:48:53 +0600

    Future proofing wavelink

--- dismusic/_version.py
+++ dismusic/_version.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-__version__ = "1.1.1a3"
+__version__ = "1.1.1a4"
 
 
 @dataclass
@@ -12,4 +12,4 @@ class VersionInfo:
     serial: int
 
 
-version_info = VersionInfo(1, 1, 1, "alpha", 3)
+version_info = VersionInfo(1, 1, 1, "alpha", 4)
