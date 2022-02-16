afc086a5af1ba611ddb3c7587d98129a87d4c431
shahriyar <mdshahriyaralam552@gmail.com>
2022-02-16 17:52:53 +0600

    Bump version to 1.1.3

--- dismusic/_version.py
+++ dismusic/_version.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-__version__ = "1.1.3b1"
+__version__ = "1.1.3"
 
 
 @dataclass
@@ -12,4 +12,4 @@ class VersionInfo:
     serial: int
 
 
-version_info = VersionInfo(1, 1, 3, "beta", 1)
+version_info = VersionInfo(1, 1, 3, "stable", 0)
