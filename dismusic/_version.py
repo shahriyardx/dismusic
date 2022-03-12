734670b739f03ba856c725e6b64aae825bb706a4
shahriyar <mdshahriyaralam552@gmail.com>
2022-03-12 11:56:44 +0600

    Dump version to 1.1.9

--- dismusic/_version.py
+++ dismusic/_version.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-__version__ = "1.1.8"
+__version__ = "1.1.9"
 
 
 @dataclass
@@ -12,4 +12,4 @@ class VersionInfo:
     serial: int
 
 
-version_info = VersionInfo(1, 1, 8, "stable", 0)
+version_info = VersionInfo(1, 1, 9, "stable", 0)
