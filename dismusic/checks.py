2d75febad82b31986f0d1961f505421b0e35556a
shahriyar <mdshahriyaralam552@gmail.com>
2022-03-21 22:04:39 +0600

    Removed unused checks

--- dismusic/checks.py
+++ dismusic/checks.py
@@ -13,29 +13,6 @@ def voice_connected():
     return commands.check(predicate)
 
 
-def player_connected():
-    def predicate(ctx: commands.Context):
-        if not ctx.voice_client:
-            raise PlayerNotConnected("Player is not connected to any voice channel.")
-
-        return True
-
-    return commands.check(predicate)
-
-
-def in_same_channel():
-    def predicate(ctx: commands.Context):
-        if not ctx.voice_client:
-            raise PlayerNotConnected("Player is not connected to any voice channel.")
-
-        if ctx.voice_client.channel.id != ctx.author.voice.channel.id:
-            raise MustBeSameChannel("You must be in the same voice channel as the player.")
-
-        return True
-
-    return commands.check(predicate)
-
-
 def voice_channel_player():
     def predicate(ctx: commands.Context):
         if not ctx.author.voice:
