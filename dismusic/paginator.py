987b10a8e44228d82e5d8338fb5fd976cddbadb2
shahriyar <mdshahriyaralam552@gmail.com>
2022-03-21 22:07:22 +0600

    Added first and last page reaction, Updated queue length parsing

--- dismusic/paginator.py
+++ dismusic/paginator.py
@@ -20,7 +20,7 @@ class Paginator:
 
         return length
 
-    def create_embed(self, tracks, current_page, total_pages, one_page=False):
+    def create_embed(self, tracks, current_page, total_pages):
         embed = Embed(color=Color(0x2F3136))
         embed.set_author(
             name="Queue",
@@ -33,21 +33,18 @@ class Paginator:
             next_song = ""
 
         description = next_song
-        queue_length = 0
-
-        for index, track in enumerate(self.player.queue._queue):
-            queue_length += track.length
-
+        queue_length = self.get_length(sum([track.length for track in self.player.queue._queue]))
+        
         for index, track in enumerate(tracks):
             description += f"{current_page * 10 + index + 1}. [{track.title}]({track.uri}) \n"
 
         embed.description = description
 
-        if one_page:
-            embed.set_footer(text=f"{len(self.player.queue._queue)} tracks, {self.get_length(queue_length)}")
+        if total_pages == 1:
+            embed.set_footer(text=f"{len(self.player.queue._queue)} tracks, {queue_length}")
         else:
             embed.set_footer(
-                text=f"Page {current_page + 1}/{total_pages}, {len(self.player.queue._queue)} tracks, {self.get_length(queue_length)}"
+                text=f"Page {current_page + 1}/{total_pages}, {len(self.player.queue._queue)} tracks, {queue_length}"
             )
 
         return embed
@@ -55,32 +52,36 @@ class Paginator:
     async def start(self):
         per_page = 10
         current_page = 0
+        track_list = list(self.player.queue._queue)
 
-        total_pages = math.ceil(len(self.player.queue._queue) / per_page)
+        total_pages = math.ceil(len(track_list) / per_page)
 
         msg = None
 
         while True:
-            track_list = list(self.player.queue._queue)
             tracks = track_list[current_page * per_page : (current_page + 1) * per_page]
-            embed = self.create_embed(tracks, current_page, total_pages, len(track_list) <= 10)
+            embed = self.create_embed(tracks, current_page, total_pages)
 
             if not msg:
                 msg = await self.ctx.send(embed=embed)
             else:
                 await msg.edit(embed=embed)
 
-            if len(track_list) > 10:
+            if total_pages > 1:
                 try:
+                    await msg.add_reaction("⏮️")
                     await msg.add_reaction("⬅️")
                     await msg.add_reaction("➡️")
-                except (HTTPException, Forbidden, NotFound, InvalidArgument):
+                    await msg.add_reaction("⏭️")
+                except (HTTPException, Forbidden, NotFound, InvalidArgument) as e:
+                    print(e)
                     pass
             else:
                 break
 
             def check(reaction, user):
-                return user == self.ctx.author and str(reaction.emoji) in ["⬅️", "➡️"] and reaction.message.id == msg.id
+                valid_reactions = ["⬅️", "➡️", "⏮️", "⏭️"]
+                return user == self.ctx.author and str(reaction.emoji) in valid_reactions and reaction.message.id == msg.id
 
             try:
                 reaction, user = await self.ctx.bot.wait_for("reaction_add", timeout=60.0, check=check)
@@ -91,5 +92,9 @@ class Paginator:
                 current_page = max(0, current_page - 1)
             elif str(reaction.emoji) == "➡️":
                 current_page = min(total_pages - 1, current_page + 1)
+            elif str(reaction.emoji) == "⏮️":
+                current_page = 0
+            elif str(reaction.emoji) == "⏭️":
+                current_page = total_pages - 1
 
             await msg.remove_reaction(reaction.emoji, user)
