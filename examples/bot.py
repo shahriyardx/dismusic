64e6c6b31ec78aa8ec0c9b412d1e1fffc89b37a8
shahriyar <mdshahriyaralam552@gmail.com>
2022-03-21 22:07:50 +0600

    Added rickroll and kids command to examples

--- examples/bot.py
+++ examples/bot.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord.ext.commands.context import Context
 
 load_dotenv(".env")
-bot = commands.Bot(command_prefix='?')
+bot = commands.Bot(command_prefix="?")
 
 bot.lavalink_nodes = [
     {"host": "losingtime.dpaste.org", "port": 2124, "password": "SleepingOnTrains"},
@@ -12,14 +13,29 @@ bot.lavalink_nodes = [
 
 # If you want to use spotify search
 bot.spotify_credentials = {
-    'client_id': 'CLIENT_ID_HERE',
-    'client_secret': 'CLIENT_SECRET_HERE'
+    "client_id": "CLIENT_ID_HERE",
+    "client_secret": "CLIENT_SECRET_HERE",
 }
+
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready")
 
-bot.load_extension('dismusic')
-bot.load_extension('jishaku')
+
+@bot.command()
+async def rickroll(ctx: Context):
+    """Never gonna give you up"""
+    await ctx.invoke(bot.get_command("play"), query="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    await ctx.send("Never gonna give you up")
+
+@bot.command()
+async def kids(ctx: Context):
+    """Test playlist playing"""
+    await ctx.invoke(bot.get_command("play"), query="https://www.youtube.com/playlist?list=PL2MHGlY_k-FG_Wc83QiOWj1-P5aXv-Tsm")
+    await ctx.send("Some kids song added for you")
+
+
+bot.load_extension("dismusic")
+bot.load_extension("jishaku")
 bot.run(os.getenv("TOKEN"))
